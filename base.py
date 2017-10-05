6b3d58a0b2e66b92776af207c612971d12b5de73
SupineKrill <CollinRWilson@gmail.com>
2017-10-04 21:40:22 -0400

    Another one

--- base.py
+++ base.py
@@ -7,7 +7,7 @@ class LoLAPI:
     def __init__(self):
         self.api_key = self.get_api() 
         self.name_regex = re.compile(r'[^0-9._ a-zA-Z\p{L}]+$')
-        self.region = "na1"
+        self.region = "kr"
         self.basic_url = f"https://{self.region}.api.riotgames.com"
 
     def get_api(self):
@@ -39,13 +39,17 @@ class LoLAPI:
     def get_summoner_id(self,summoner_name, return_json=True):
         """Get a summoner by account id. Return JSON"""
         if self.real_name(summoner_name):
-            return self.request_json(f"/lol/summoner/v3/summoners/by-name/{summoner_name}")
+            return self.request_json(f"/lol/summoner/v3/summoners/by-name/{summoner_name}", return_json)
 
     def get_ranked_games(self, account_id, return_json=True):
         """Takes account ID. Returns JSON of all ranked games played"""
-        return self.request_json(f"/lol/match/v3/matchlists/by-account/{account_id}", False) 
+        return self.request_json(f"/lol/match/v3/matchlists/by-account/{account_id}", return_json) 
+
+    def get_last_20(self, account_id, return_json=True):
+            return self.request_json(f"/lol/match/v3/matchlists/by-account/{account_id}/recent", return_json)
 
 if __name__ == '__main__':
     test = LoLAPI()
-    self_id = test.get_summoner_id("SupineKrill")["accountId"]
+    self_id = test.get_summoner_id("멘탈잡자으쌰으쌰", True)
     print(self_id)
+    print(test.get_last_20(self_id["accountId"]))
